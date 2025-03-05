067929454e079c3cad23b99e55da579a258a1560
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-04 15:43:56 -1000

    Update mat_transpose.c

--- mat_transpose.c
+++ mat_transpose.c
@@ -1,17 +1,10 @@
 /*
-III. BONUS - Transpose a matrix (15 points)
+III. BONUS - Deletion of an element of an array (20 points)
 
-Write a C program that allows to transpose a matrix.
-The transpose of a matrix is a new matrix that is obtained by exchanging the rows and columns.
-The user provides the size of the matrix (number of rows and columns).
-The user is then asked to provide the elements of the matrix. 
-The program then computes the transpose of the matrix and prints the resulting matrix.
-
-Note:
-1. Input integers in the following order only:
-    - size of the array (rows and then columns)
-    - elements of the array
-2. Display elements of the transposed matrix using the following printf format only:
-    printf("%4d", <elements of transposed matrix>)
+Write a C program that allows a user to delete an element from a specified position in an array.
+The program should first prompt the user to enter the size of the array and then input elements in
+ascending order. The user should then provide the position of the element to be deleted, and the
+program should update the array accordingly by shifting elements to the left. Finally, the program
+should display the modified array.
 */
 
