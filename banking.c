b93269f73d8fe542de95b540854d27ad60a9c08e
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-04 15:39:10 -1000

    Update and rename area_calc.c to banking.c

---
+++ banking.c
@@ -0,0 +1,19 @@
+/*
+I. Simple Banking System
+
+Write a C program that simulates a simple banking system. The program should allow multiple
+users to register, being authenticated and perform the following operations:
+1. Check balance,
+2. Deposit funds,
+3. Withdraw funds,
+4. Exit.
+
+** Important:
+- Register users with a username (max 20 characters), 4-digit PIN, and an initial balance of $0.00.
+- Authenticate username and PIN (both should be correct for successful authentication).
+- Create function for each operation: registration, authentication, check balance, deposit, withdraw.
+- Keep track of user's balance and dispaly updated balance after each transcation.
+- Menu choice after each action (1-4) and error message otherwise
+- Ensure that withdrawals cannot exceed the available balance.
+- Use good input/output prompts and error messages for invalid authentication attempts and incorrect menu choices.
+*/
