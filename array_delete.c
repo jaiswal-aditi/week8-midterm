aaf454cc58daea6c34fde540acf0c2173de69667
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-04 15:48:17 -1000

    Update and rename mat_transpose.c to array_delete.c

--- array_delete.c
+++ array_delete.c
@@ -6,5 +6,20 @@ The program should first prompt the user to enter the size of the array and then
 ascending order. The user should then provide the position of the element to be deleted, and the
 program should update the array accordingly by shifting elements to the left. Finally, the program
 should display the modified array.
+
+Example of Input and Output:
+
+Input:
+Input the size of the array: 5
+Input 5 elements in the array in ascending order:
+element at index 0: 10
+element at index 1: 20
+element at index 2: 30
+element at index 3: 40
+element at index 4: 50
+Input the position of the element to delete: 3
+
+Output:
+The new list is: 10 20 40 50
 */
 
