b99ffaeaa9bacbbf8cc376038f09a52f1b21c959
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-04 15:50:08 -1000

    Create hanoi_tower.c

---
+++ hanoi_tower.c
@@ -0,0 +1,24 @@
+/*
+IV. Bonus - Tower of Hanoi (35 points)
+
+The Tower of Hanoi is a mathematical puzzle that consists of three rods and a number of disks of
+different sizes which can slide onto any rods (see diagram in the pdf).
+
+The puzzle starts with the disks in ascending order of size on one rod, the smallest at the top, thus making a conical shape.
+The objective of the puzzle is to move all disks from the source rod to the destination rod while following the rules below:
+• Only one disk can be moved at a time.
+• Each move consists of taking the top disk from one of rod and placing it on another rod (i.e., a disk can only be moved if it is the top disk on a rod.).
+• No disk may be placed on top of a smaller disk.
+
+Sample Inputs/Outputs:
+
+Enter the number of disks: 3
+Solution for Tower of Hanoi with 3 disks:
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+*/
