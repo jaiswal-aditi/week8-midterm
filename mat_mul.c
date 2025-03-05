20ceeed6ccc52f067aa076e7a8c41a81e1ed9f1f
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-04 15:42:34 -1000

    Update mat_mul.c

--- mat_mul.c
+++ mat_mul.c
@@ -1,31 +1,27 @@
 /*
-I. Multiplication of two matrices (45 points)
+II. Degrees Fahrenheit to Celsius (15 points)
 
-Write a C program that asks the user to enter the size (number of rows and columns) of two matrices.
-- To multiply two matrices, the number of columns of the first matrix needs to be equal to the number 
-    of rows of the second matrix. The program asks for the number of rows and columns until the above 
-    condition is satisfied.
-- Then, the program performs the multiplication of two matrices and display the resulting on the screen.
-    To perform this, you will must use multiple functions:
-    1. one to take the matrix elements provided by the user,
-    2. another to multiply the two matrices 
-    3. and one to display the resulting matrix after multiplication.
+Write a C program that prints a table converting temperatures from degrees Fahrenheit to degrees
+Celsius using the following formula:
+            ℃ = (5 9⁄ ) ∗ (℉ − 32)
 
-Note:
-1. Input integers in the following order only:
-    - size of the two arrays
-    - elements of the two arrays
-2. Display elements of the resulting matrix using the following printf format only:
-    printf("%4d", <elements of resulting matrix>)
+The table should start at 1°F, then include 20°F, and increment the following temperatures by 20°F up to 300°F.
 
-Example:
-Enter rows and column for the first matrix: 2 2
-Enter rows and column for the second matrix: 2 2
-
-Elements of the first matrix: 1 2 3 4
-Elements of the second matrix: 1 2 3 4
-
-Result of matrix multiplication:
-   7   10 
-  15   22
+                Fahrenheit    Celsius
+                    1           -17
+                    20          -6
+                    40           4
+                    60           15
+                    80           26
+                    100          37
+                    120          48
+                    140          60
+                    160          71
+                    180          82
+                    200          93
+                    220          104
+                    240          115
+                    260          126
+                    280          137
+                    300          148
 */
